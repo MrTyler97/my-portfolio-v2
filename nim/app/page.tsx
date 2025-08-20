d4646bf084fce3ed986eb333a021d8145d73d540
Victor Tyler <146302984+MrTyler97@users.noreply.github.com>
2025-08-20 17:46:09 -0400

    final change

--- nim/app/page.tsx
+++ nim/app/page.tsx
@@ -442,11 +442,11 @@ export default function Personal() {
         <div className="relative inline-block">
           <div className="pointer-events-none absolute -inset-2">
             <GlowEffect
-              colors={['#0894FF', '#C959DD', '#ff2e2eff', '#04ff5cff']}
+              colors={['#ff5a08ff', '#ff2e2eff','#C959DD']}
               mode="rotate"
               blur="strongest"
               duration={9}
-              scale={0.9}
+              scale={.9}
             />
           </div>
           <div className="relative flex flex-col items-start space-y-3 rounded-2xl border border-zinc-300/40 bg-zinc-100 p-6 dark:border-zinc-700/40 dark:bg-zinc-900">
